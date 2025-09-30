d0b9da754166affad5e6efeb8fa21e96dbd36eb5
Keith Kurak <keith.kurak@gmail.com>
2025-09-30 11:35:26 -0400

    create branch script

---
+++ create-branch.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+const { execSync } = require("child_process");
+
+function run(command) {
+  try {
+    console.log(`$ ${command}`);
+    return execSync(command, { stdio: "inherit" });
+  } catch (err) {
+    console.error(`Error running command: ${command}`);
+    process.exit(1);
+  }
+}
+
+const branchName = process.argv[2];
+
+if (!branchName) {
+  console.error("Usage: node create-branch.js <branch-name>");
+  process.exit(1);
+}
+
+// Create and switch to the branch
+run(`git checkout -b ${branchName}`);
+
+// Make an empty commit with the branch name as the message
+run(`git commit --allow-empty -m "${branchName}"`);
